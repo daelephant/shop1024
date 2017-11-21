778f202370398e37c5b8ebafef6c1376215cfb8b
family <2224168716@qq.com>
2017-11-21 22:06:23 +0800

    1114

--- shop/Runtime/Cache/Back/0e2996a3ef19482b3585a60b0fd71b19.php
+++ shop/Runtime/Cache/Back/0e2996a3ef19482b3585a60b0fd71b19.php
@@ -21,9 +21,48 @@
             </span>
 </div>
 <div></div>
+ <div style="font-size: 13px; margin: 10px 5px;">
+     按商品类型进行显示属性:
+
+     <select id="type_id" onchange="show_attr()">
+         <?php if(is_array($typeinfo)): foreach($typeinfo as $key=>$v): if(($_GET['type_id']) == $v['type_id']): ?><option value="<?php echo ($v["type_id"]); ?>" selected="selected">
+              <?php else: ?>
+                 <option value="<?php echo ($v["type_id"]); ?>"><?php endif; ?>
+             <?php echo ($v["type_name"]); ?></option><?php endforeach; endif; ?>
+     </select>
+     <script type="text/javascript">
+         function show_attr() {
+             var type_id = $('#type_id option:selected').val();//获得被选中的value值
+             // console.log(type_id);//ok
+             //利用ajax去服务器端获取“选中类型”对应的属性信息
+             $.ajax({
+                 url:"<?php echo U('Attribute/getInfoByType');?>",
+                 data:{'type_id':type_id},
+                 dataType:'json',
+                 type:'get',
+                 success:function (msg) {
+                     //遍历msg中的每个属性信息并显示
+                     var s = "";
+                     $.each(msg,function (k,v) {
+                         s+="<tr>";
+                         s+="<td>"+v.attr_id+"</td>";
+                         s+="<td>"+v.attr_name+"</td>";
+                         s+="<td>"+v.type_id+"</td>";
+                         s+="<td>"+v.attr_is_sel+"</td>";
+                         s+="<td>"+v.attr_write_mod+"</td>";
+                         s+="<td>"+v.attr_sel_opt+"</td>";
+                         //<td><a href="/index.php/Back/Attribute/upd/auth_id/<?php echo ($v['type_id']); ?>" >修改</a></td
+                         s+='<td><a href="/index.php/Back/Attribute/upd/auth_id/'+v.type_id+'" >修改</a></td>';
+                         s+="</tr>";
+                     });
 
 
-        <div style="font-size: 13px; margin: 10px 5px;">
+                     $('.table_a tr:not(:first)').remove();
+                     $('.table_a').append(s);
+                 }
+         });
+         }
+     </script>
             <table class="table_a" border="1" width="100%">
                 <tbody><tr style="font-weight: bold;">
                         <td>序号</td>
